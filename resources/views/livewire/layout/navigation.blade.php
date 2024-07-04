0f3dad1c5cd82e315fe582173cae31e808173e6f
Ismael FINNAOUI <ifinnaoui@gmail.com>
2024-07-04 23:55:03 +0200

    update billing-portal in livewire component

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -56,11 +56,7 @@ new class extends Component {
                 @if ($isSubscribed)
                     <li>
                         <a href="{{ route('billing-portal') }}" target="_blank">
-<<<<<<< HEAD
-                            Billing portal : Manage your subscription
-=======
                             Billing portal
->>>>>>> refs/remotes/origin/main
                         </a>
                     </li>
                 @endif
