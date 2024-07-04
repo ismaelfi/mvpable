59ae6e7f077824f8293b1d7812a7b89e0018f1ee
Ismael FINNAOUI <ifinnaoui@gmail.com>
2024-07-04 22:51:47 +0200

    update pricing card AppLayout

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -12,15 +12,15 @@
                 @foreach ($plans as $plan)
                     <div class="card bg-base-100 w-full shadow-xl">
                         <div class="card-body">
-                            <h3 id="tier-{{ $plan->id }}" class="card-title text-2xl font-bold text-indigo-400">{{ $plan->name }}</h3>
-                            <p class="text-5xl font-bold tracking-tight text-gray-900">${{ $plan->price }}<span class="text-base text-gray-700">/month</span></p>
-                            <p class="mt-4 text-base leading-7 text-gray-800">{{ $plan->description }}</p>
+                            <h3 id="tier-{{ $plan->id }}" class="card-title text-2xl font-bold text-base-content">{{ $plan->name }}</h3>
+                            <p class="text-5xl font-bold tracking-tight text-base-content">${{ $plan->price }}<span class="text-base">/month</span></p>
+                            <p class="mt-4 text-base leading-7 text-base-content">{{ $plan->description }}</p>
                             @if (is_array($plan->features) && count($plan->features) > 0)
-                                <ul class="mt-4 space-y-3 text-sm leading-6 text-gray-700">
+                                <ul class="mt-4 space-y-3 text-sm leading-6 text-base-content">
                                     @foreach ($plan->features as $feature)
                                         @if (isset($feature['feature_name']))
                                             <li class="flex gap-x-3">
-                                                <svg class="h-6 w-5 flex-none text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                <svg class="h-6 w-5 flex-none text-primary" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                     <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd"></path>
                                                 </svg>
                                                 {{ $feature['feature_name'] }}
