d5412599f335ad23c927bdb97f58175745984394
Ismael FINNAOUI <ifinnaoui@gmail.com>
2024-07-04 22:25:37 +0200

    update label color

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-primary-content']) }}>
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm']) }}>
     {{ $value ?? $slot }}
 </label>
