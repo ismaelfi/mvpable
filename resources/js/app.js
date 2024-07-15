f731ff8add69a901b97db5a6e039b4e89a48263c
Ismael FINNAOUI <ifinnaoui@gmail.com>
2024-07-15 14:54:08 +0200

    Disable Alpine.js to fix wire:navigate conflicts
    Temporarily commented out Alpine.js initialization in app.js to resolve
    conflicts with Livewire's wire:navigate feature. Added explanatory comment
    for future reference

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
 import './bootstrap';
-import Alpine from 'alpinejs'
 
-window.Alpine = Alpine
+// The following Alpine.js initialization is currently commented out
+// due to conflicts with Livewire's wire:navigate feature.
+// If you need Alpine.js functionality, you'll need to find a way to
+// integrate it with Livewire that doesn't interfere with navigation.
+// TODO: Investigate proper integration of Alpine.js with Livewire 3.x
 
-Alpine.start()
+// import Alpine from 'alpinejs'
+// window.Alpine = Alpine
+// Alpine.start()
